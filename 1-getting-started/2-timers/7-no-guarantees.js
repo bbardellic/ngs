d3a8184ccdc875e214ace102852c02a43214a5a9
bruno <bbardellic@gmail.com>
2019-08-31 15:49:17 -0500

    Challenge 2 Hello after 0.5 seconds. MAYBE!

--- 1-getting-started/2-timers/7-no-guarantees.js
+++ 1-getting-started/2-timers/7-no-guarantees.js
@@ -1,8 +1,17 @@
-setTimeout(
-  () => console.log('Hello after 0.5 seconds. MAYBE!'),
-  500,
-);
+const test =  () => console.log('Hello after 0.5 seconds. MAYBE!');
 
-for (let i = 0; i < 1e10; i++) {
-  // Block Node Synchronously
+// const test2 = sec => {
+//   setTimeout(() => console.log('Hello after 0.5 seconds. MAYBE!'), 500 * sec);
+// }
+
+
+for (let i = 1; i <= 50; i++) {
+  //console.log(i)
+  setTimeout(test, 500 * i)
+  
+  
+  
+  //test2;
 }
+
+//Intenté usar un colas https://www.npmjs.com/package/queue pero no logré correr el ejemplo
